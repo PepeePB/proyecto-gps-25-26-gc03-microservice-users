e3d6ea993774338697954bec3780d3a0dd6c2215
Samuel Soto Paredes <ssotopar@alumnos.unex.es>
2025-10-27 18:32:16 +0100

    Project base created

---
+++ src/test/java/es/musicfly/microserviciousuarios/MicroservicioUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package es.musicfly.microserviciousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioUsuariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
